55bef31213473635da3e21aa209a51d9060cb686
m0nds <m0nds@m0nds-MacBook-Pro.local>
2024-04-10 22:32:41 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import Model from "./components/Model"
+
+const App = () => {
+  return (
+    <div className='bg-black'>
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+      <Model/>
+    </div>
+  )
+}
+
+export default App
